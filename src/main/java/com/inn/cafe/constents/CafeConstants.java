5aa851a0802074c867d58d4620a91a5ece9ab3a8
Khang <nmkhang130901@gmail.com>
2024-03-08 20:34:47 +0700

    Bill

--- src/main/java/com/inn/cafe/constents/CafeConstants.java
+++ src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -6,4 +6,7 @@ public class CafeConstants {
     public static final String INVALID_DATA = "Invalid Data.";
 
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String STORE_LOCATION = "D:\\DACNTT2\\storedFiles\\cafe_stored_files";
+
 }
